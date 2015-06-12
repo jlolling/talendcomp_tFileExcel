286ee01f264ff3fca827d6ebff80b230c5d7987b
janlolling <janlolling@0b83e5a7-efa2-48d3-b164-a0b6d2bda42a>
2015-06-12 17:06:56 +0000

    release 6.10

--- talendcomp_tFileExcel/src/main/java/de/cimt/talendcomp/tfileexcelpoi/SpreadsheetInput.java
+++ talendcomp_tFileExcel/src/main/java/de/cimt/talendcomp/tfileexcelpoi/SpreadsheetInput.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 Jan Lolling jan.lolling@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.cimt.talendcomp.tfileexcelpoi;
 
 import java.math.BigDecimal;
@@ -652,11 +667,11 @@ public class SpreadsheetInput extends SpreadsheetFile {
 		if (sheetName == null || sheetName.trim().isEmpty()) {
 			throw new Exception("Name of sheet cannot be null or empty!");
 		}
-		targetSheetName = sheetName;
-		sheet = workbook.getSheet(targetSheetName);
+		sheet = workbook.getSheet(sheetName);
 		if (sheet == null) {
 			throw new Exception("Sheet with name:" + targetSheetName + " does not exists!");
 		}
+		targetSheetName = sheetName;
 		currentDatasetNumber = 0;
 		sheetLastRowIndex = 0;
 		lastValueMap = new HashMap<Integer, Object>();
@@ -674,6 +689,7 @@ public class SpreadsheetInput extends SpreadsheetFile {
 		if (sheet == null) {
 			throw new Exception("Sheet with index:" + index + " does not exists!");
 		}
+		targetSheetName = sheet.getSheetName();
 		currentDatasetNumber = 0;
 		sheetLastRowIndex = 0;
 		lastValueMap = new HashMap<Integer, Object>();
