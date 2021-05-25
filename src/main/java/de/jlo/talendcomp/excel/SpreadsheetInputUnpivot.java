a2ce1efd8fe6b9413f8370d0b9d7de5b60d04724
Jan Lolling <jan.lolling@xom-materials.com>
2021-05-25 11:59:39 +0200

    keep the current state of work

--- src/main/java/de/jlo/talendcomp/excel/SpreadsheetInputUnpivot.java
+++ src/main/java/de/jlo/talendcomp/excel/SpreadsheetInputUnpivot.java
@@ -1,19 +1,81 @@
 package de.jlo.talendcomp.excel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.poi.ss.usermodel.Row;
 
 public class SpreadsheetInputUnpivot {
 	
-	private SpreadsheetInput input = null;
-	private int unpivotColumnRangeStartIndex = 0;
-	private int unpivotColumnRangeEndIndex = 0;
-	private Row headerRow = null;
-	private Row currentRow = null;
-	
-	public void setInput(SpreadsheetInput input) {
-		this.input = input;
+	/**
+	 * This class holds the normalized values 
+	 * @author jan.lolling@gmail.com
+	 *
+	 */
+	public static class NormalizedRow {
+		
+		private int rowIndex = 0;
+		private int originalColumnIndex = 0;
+		private String headerName = null;
+		private Object value = null;
+		
+		public int getRowIndex() {
+			return rowIndex;
+		}
+		
+		public void setRowIndex(int rowIndex) {
+			this.rowIndex = rowIndex;
+		}
+		
+		public int getOriginalColumnIndex() {
+			return originalColumnIndex;
+		}
+		
+		public void setOriginalColumnIndex(int originalColumnIndex) {
+			this.originalColumnIndex = originalColumnIndex;
+		}
+		
+		public String getHeaderName() {
+			return headerName;
+		}
+		
+		public void setHeaderName(String headerName) {
+			if (headerName == null || headerName.trim().isEmpty()) {
+				throw new IllegalArgumentException("headerName cannot be null or empty");
+			}
+			this.headerName = headerName;
+		}
+		
+		public Object getValue() {
+			return value;
+		}
+		
+		public void setValue(Object value) {
+			this.value = value;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof NormalizedRow) {
+				return ((NormalizedRow) o).headerName.equals(headerName);
+			} else {
+				return false;
+			}
+		}
+		
+		@Override
+		public int hashCode() {
+			return headerName.hashCode();
+		}
+		
 	}
 
+	private SpreadsheetInput spreadsheetInput = null;
+	private int unpivotColumnRangeStartIndex = 0;
+	private int unpivotColumnRangeEndIndex = 0;
+	private int headerRowIndex = 0;
+	private List<String> headerNames = new ArrayList<>();
+	
 	public int getUnpivotColumnRangeStartIndex() {
 		return unpivotColumnRangeStartIndex;
 	}
@@ -30,22 +92,36 @@ public class SpreadsheetInputUnpivot {
 		this.unpivotColumnRangeEndIndex = unpivotColumnRangeEndIndex;
 	}
 
-	public Row getHeaderRow() {
-		return headerRow;
+
+	public void setSpreadsheetInput(SpreadsheetInput spreadsheetInput) {
+		if (spreadsheetInput == null) {
+			throw new IllegalArgumentException("The reference to the component tFileExcelSheetInput cannot be null");
+		}
+		this.spreadsheetInput = spreadsheetInput;
+	}
+	
+	private void setupHeaderNames() {
+		Row headerRow = spreadsheetInput.getRow(headerRowIndex);
+		int cellIndex = unpivotColumnRangeStartIndex;
+		boolean foundHeaderName = true;
+		while (foundHeaderName) {
+			try {
+				String name = spreadsheetInput.getStringCellValue(cellIndex, true, true, false);
+			} catch (Exception e) {
+				
+			}
+		}
 	}
 
-	public void setHeaderRow(Row headerRow) {
-		this.headerRow = headerRow;
+	public int getHeaderRowIndex() {
+		return headerRowIndex;
 	}
 
-	public Row getCurrentRow() {
-		return currentRow;
+	public void setHeaderRowIndex(Integer headerRowIndex) {
+		if (headerRowIndex != null && headerRowIndex > 0) {
+			this.headerRowIndex = headerRowIndex;
+		}
 	}
-
-	public void setCurrentRow(Row currentRow) {
-		this.currentRow = currentRow;
-	}
-
 	
 	
 }
